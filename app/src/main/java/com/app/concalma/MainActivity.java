664b47783c3bf1dd4cd9189c2311dab8a579c237
junecubes <junecubes@gmail.com>
2019-08-04 02:00:28 +0530

    5

--- app/src/main/java/com/app/concalma/MainActivity.java
+++ app/src/main/java/com/app/concalma/MainActivity.java
@@ -3,11 +3,20 @@ package com.app.concalma;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.startapp.android.publish.adsCommon.StartAppAd;
+import com.startapp.android.publish.adsCommon.StartAppSDK;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartAppSDK.init(this, "207067053", true);
+    }
+    public void onBackPressed() {
+        StartAppAd.onBackPressed(this);
+        super.onBackPressed();
     }
 }
